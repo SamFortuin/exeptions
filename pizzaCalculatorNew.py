075db00c14600fd29afb9463fa6ba6c63e23ca03
Sam Fortuin <99059050@mydavinci.nl>
2022-03-16 10:29:16 +0100

    unstupified reciept

--- pizzaCalculatorNew.py
+++ pizzaCalculatorNew.py
@@ -1,10 +1,15 @@
-# from shortcuts import *
-from string import capwords
+#99059050, Sam Fortuin, pizzaCalculator
 from time import sleep
+from sys import stdout
 
 def main():
     #vars
-    smallPizzaAmount = int(input("How many small pizza's do you want?\n"))
+    smallPizzaAmount = input("How many small pizza's do you want?\n")
+    try:
+        smallPizzaAmount = int(smallPizzaAmount)
+    except ValueError:
+        print('not a number')
+        main()
     mediumPizzaAmount = int(input("How many medium pizza's do you want?\n"))
     largePizzaAmount = int(input("How many large pizza's do you want?\n"))
     #prices in €, based on NYPizza
@@ -17,27 +22,26 @@ def main():
     largePizzaTotal = largePizzaAmount * largePizzaPrice
     totalPrice = smallPizzaTotal + mediumPizzaTotal + largePizzaTotal
     
-    #print final calculator line
-    print("\n\n--------------------\nYour order:\n--------------------\n\n"+str(smallPizzaAmount),"small pizza's totaling €"+str(round(smallPizzaTotal,2))+"\n"+str(mediumPizzaAmount),"medium pizza's totaling €"+str(round(mediumPizzaTotal,2))+"\n"+str(largePizzaAmount),"large pizza's totaling €"+str(round(largePizzaTotal,2)),"\n\n--------------------\nOrder Total: €"+str(round(totalPrice,2))+"\n--------------------\n")
+    #reciept
+    stripe = '---------------------'
+    smallPizz = f"\n{smallPizzaAmount} small  pizza's totaling €{str(round(smallPizzaTotal,2)).replace('.',',')}" if smallPizzaAmount > 0 else ''
+    mediumPizz = f"\n{mediumPizzaAmount} medium  pizza's totaling €{str(round(mediumPizzaTotal,2)).replace('.',',')}" if mediumPizzaAmount > 0 else ''
+    largePizz = f"\n{largePizzaAmount} large  pizza's totaling €{str(round(largePizzaTotal,2)).replace('.',',')}" if largePizzaAmount > 0 else ''
     
-    #asks if another order after 500ms & lowers it
-    sleep(0.5)
-    restart = input("Do you want to buy more pizza's? Y/N ").lower()[:1]
+    print(f"{stripe}\n     Your order:\n{stripe}{smallPizz}{mediumPizz}{largePizz}\
+    \n{stripe}\nOrder total:  €{str(round(totalPrice,2)).replace('.',',')}\n{stripe}\n")
+    
+    # \n{mediumPizzaAmount} medium pizza's totaling €{str(round(mediumPizzaTotal,2)).replace('.',',')}\
+    # \n{largePizzaAmount} large  pizza's totaling €{str(round(largePizzaTotal,2)).replace('.',',')}\
+    
 
-    #if yes is typed program restarts itself
+    restart = input("Do you want to buy more pizza's? Y/N ").lower()[:1].replace('j','y')
     if restart == "y":
         main()
-    #if no is typed exits with ty msg
     elif restart == "n":
         print("Thank you for using the calculator. program will now exit")
-        sleep(0.2)
-        exit()
     else:
-        print("Input not regonized as any form of yes or no. program will now exit")
-        sleep(0.2)
-        exit()
+        print('a')
 
 if __name__ == "__main__":
     main()
-
-#add name for the order
